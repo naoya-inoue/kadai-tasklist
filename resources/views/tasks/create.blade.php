7eb7a621aa085b05cbee5fd2fa8dc7baaf20e479
naoya.inoue <a.good.for.nothing.fellow@gmail.com>
2018-11-22 13:13:06 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,17 +3,31 @@
 @section('content')
 
     <h1>新規タスクの登録ページ</h1>
-    
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('subject', '件名:') !!}
-        {!! Form::text('subject') !!}
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-3 col-lg-6"> 
+ 
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
-        {!! Form::label('status', 'ステータス：') !!}
-        {!! Form::text('status') !!}        
+            <div class="form-group">
+                {!! Form::label('subject', '件名:') !!}
+                {!! Form::text('subject', null, ['class' => 'form-control']) !!}
+            </div>
+            
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス：') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}        
+            </div>
+            
+            {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+            
+        {!! Form::close() !!}
         
-        {!! Form::submit('登録') !!}
-        
-    {!! Form::close() !!}
-
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,17 +3,31 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-3 col-lg-6"> 
     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         
+        <div class="form-group">
         {!! Form::label('subject', '件名：') !!}
-        {!! Form::text('subject') !!}
+        {!! Form::text('subject', null, ['class' => 'form-control']) !!}
+        </div>
 
+        <div class="form-group">
         {!! Form::label('status', 'ステータス') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
         
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
         
     {!! Form::close() !!}
 
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
 @endsection
